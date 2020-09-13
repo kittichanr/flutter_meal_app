15e485ce98946b0f1182d90547c23d7fa01bfaed
Petch <petchkubb@live.com>
2020-09-13 07:32:57 +0700

    add base data

---
+++ lib/dummy_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(id: 'c1', title: 'Intalian', color: Colors.purple),
+  Category(id: 'c2', title: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', title: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c4', title: 'German', color: Colors.blue),
+  Category(id: 'c5', title: 'Thai', color: Colors.green),
+  Category(id: 'c6', title: 'American', color: Colors.amber),
+  Category(id: 'c7', title: 'Japan', color: Colors.pink),
+  Category(id: 'c8', title: 'Vietnam', color: Colors.indigo),
+  Category(id: 'c9', title: 'China', color: Colors.grey),
+  Category(id: 'c10', title: 'French', color: Colors.lime),
+];
